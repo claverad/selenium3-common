9ca5b6d5e554a2c30ac5a399068312ffd5ab4848
claverad <claudioverad@gmail.com>
2021-04-12 10:39:03 -0400

    se crea pry base

---
+++ src/main/java/excel/PcfactoryExcel.java
@@ -0,0 +1,16 @@
+package excel;
+
+public class PcfactoryExcel {
+
+    private String nombrExcel= "resultadoPcfactory";
+    AccionesExcel excel = new AccionesExcel();
+
+    public void excelPcfactory(String dato[]){
+
+        if(!excel.existeExcel(nombrExcel)){
+            String [] cabeceraPcfactory = {"Id_Producto", "nombre_producto", "Marca", "Precio_referencial", "precio_oferta_efec", "url"};
+            excel.crearNuevoExcel(nombrExcel, "Producto",cabeceraPcfactory);
+        }
+        excel.agregarLineaExcel(nombrExcel,dato);
+    }
+}
